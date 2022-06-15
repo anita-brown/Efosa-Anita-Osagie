238abe7f837c62df599efe23617636f01ef73f4d
anita-brown <anita.osagie@decagon.dev>
2022-06-15 16:01:21 +0100

    data created

---
+++ assessment/src/data.js
@@ -0,0 +1,14 @@
+export const a = {
+  user: {
+    id: 1,
+    name: {
+      firstName: "James",
+      lastName: "Ibori",
+    },
+    location: {
+      city: "Ikoyi",
+      state: "Lagos",
+      address: "One expensive house like that",
+    },
+  },
+};
